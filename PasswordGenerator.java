445a6ff919a17168c8cbc7702b2914587d2a12f2
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-02-04 19:34:44 +0300

    Add files via upload

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,2 +1,39 @@
+import java.util.Random;
+
+//this now works as the backend/logic of our program
 public class PasswordGenerator {
+    //character pools
+    public static final String LOWERCASE_CHARACTERS="abcdefghijklmnopqrstuvwxyz";
+    public static final String UPPERCASE_CHARACTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String NUMBERS="0123456789";
+    public static final String SPECIAL_SYMBOLS="!@#$%^&*()+=_-{}[];:,.<>/?";
+
+
+    //the random class helps us to generate a random number which will be used to randomly choose the characters
+
+    private final Random random;
+    //constructor
+    public PasswordGenerator(){random=new Random();}
+    public String generatePassword(int length,boolean includeUppercase,boolean includeLowercase,boolean includeNumbers,boolean includeSpecialSymbols){
+        StringBuilder passwordBuilder=new StringBuilder();
+        //store valid characters
+        String validCharacters="";
+        if (includeUppercase)validCharacters+= UPPERCASE_CHARACTERS;
+        if (includeLowercase)validCharacters += LOWERCASE_CHARACTERS;
+        if (includeNumbers)validCharacters += NUMBERS;
+        if (includeSpecialSymbols)validCharacters += SPECIAL_SYMBOLS;
+        //build password
+        for (int i=0;i<length;i++){
+            //generate a random index
+            int randomIndex=random.nextInt(validCharacters.length());
+
+            //get the character based on the random index
+            char randomChar=validCharacters.charAt(randomIndex);
+            //store char into password builder
+            passwordBuilder.append(randomChar);
+            //do this until it meets the length of the user
+        }
+        //return the result
+        return passwordBuilder.toString();
+    }
 }
--- PasswordGeneratorGUI.java
+++ PasswordGeneratorGUI.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //render the gui components(frontend)
 //this class will inherit from the JFrame class
 public class PasswordGeneratorGUI extends JFrame {
+    private PasswordGenerator passwordGenerator;
     public PasswordGeneratorGUI(){
         //render frame and a title
         super("Password Generator");
@@ -13,6 +16,8 @@ public class PasswordGeneratorGUI extends JFrame {
         setLayout(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        //initialize password generator
+        passwordGenerator=new PasswordGenerator();
 
         //add gui components
        addGuiComponents();
@@ -77,6 +82,28 @@ public class PasswordGeneratorGUI extends JFrame {
         JButton generateButton =new JButton("Gernerate");
         generateButton.setFont(new Font("Dialog",Font.PLAIN,32));
         generateButton.setBounds(155,477,222,41);
+        generateButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+             //validation:generate a password only when length >0 and one of the toggled buttons is pressed
+                if (passwordLengthInputArea.getText().length() <= 0 )return;
+                boolean anyToggleSelected =lowercaseToggle.isSelected()||
+                        uppercaseToggle.isSelected()||
+                        numbersToggle.isSelected()||
+                        symbolsToggle.isSelected();
+                //generate password
+                int passwordLength=Integer.parseInt(passwordLengthInputArea.getText());
+                if (anyToggleSelected){
+                    String generatePassword =passwordGenerator.generatePassword(passwordLength,uppercaseToggle.isSelected(),
+                            lowercaseToggle.isSelected(),
+                            numbersToggle.isSelected(),
+                            symbolsToggle.isSelected()
+                            );
+                    //display the password
+                    passwordOutput.setText(generatePassword);
+                }
+            }
+        });
         add(generateButton);
     }
 
